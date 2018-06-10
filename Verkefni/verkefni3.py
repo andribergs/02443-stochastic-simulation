07d12f6c08d6ce7a7201bbcb96857f0214a6005c
IceTharu <thj3@hi.is>
2018-06-10 17:19:14 +0200

    hæhæ

--- Verkefni/verkefni3.py
+++ Verkefni/verkefni3.py
@@ -7,7 +7,7 @@ Created on Wed Jun  6 11:56:27 2018
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import pareto, norm, expon
+from scipy.stats import pareto, norm, expon, t
 
 #%%
 #---- Exponential Distribution ----
@@ -36,7 +36,7 @@ xmin, xmax = plt.xlim()
 plt.plot(x, expon.pdf(x),'g-', lw=2,alpha=0.6)
 plt.title("Exponentially Distributed Histogram")
 plt.xlabel("Classes")
-plt.ylabel("Frequency")
+plt.ylabel("Density")
 plt.show
 #%%
 #---- Normal Distribution ----
@@ -78,7 +78,7 @@ p = norm.pdf(x, mud, stdd)
 plt.plot(x, p,'g-', lw=2,alpha=0.6)
 plt.title("Normal Distributed Histogram")
 plt.xlabel("Classes")
-plt.ylabel("Frequency")
+plt.ylabel("Density")
 plt.show
 
 #%% 
@@ -90,7 +90,7 @@ m = 100 # 100 Number of intervals
 for i in range(m):
     x = normo(0,1,n)
     x_bar = np.mean(x)
-    z_star = stats.t.ppf(0.975,n-1)
+    z_star = t.ppf(0.975,n-1)
     upper = x_bar + z_star*(np.std(x)/np.sqrt(n))
     lower = x_bar - z_star*(np.std(x)/np.sqrt(n))
     ulim = np.append(ulim,upper)
@@ -121,7 +121,7 @@ mean value and variance, with analytical results.
 def paretobay(beta,k,n):
     U = np.random.uniform(0,1,n)
     res = beta*(U**(-1/k)-1)
-    mean = (k/(k-1))*beta
+    mean = (k/(k-1))*beta-1
     variance = (k/((k-1)**2*(k-2)))*beta**2
     return res, mean, variance
 
@@ -137,13 +137,14 @@ anavar1 = np.var(res31)
 x1 = np.linspace(pareto.ppf(0.01, k1),pareto.ppf(0.99,k1),100)
 
 plt.figure()
-plt.hist(res31,align='mid',color='tan',edgecolor='moccasin',bins=100,density=True,stacked=True)
+plt.hist(res31,align='mid',color='tan',edgecolor='moccasin',bins=20,density=True,stacked=True)
 xmin, xmax = plt.xlim()
 ymin, ymax = plt.ylim()
 plt.plot(x1-1, pareto.pdf(x1, k1),'g-', lw=2,alpha=0.6)
+plt.ylim(ymin,ymax)
 plt.title("Pareto Distributed Histogram (k=2.05)")
 plt.xlabel("Classes")
-plt.ylabel("Frequency")
+plt.ylabel("Density")
 plt.show
 print('----Pareto with K = 2.05----')
 print('The theoretical mean is: {0}'.format(mean1))
@@ -159,13 +160,14 @@ anavar2 = np.var(res32)
 x2 = np.linspace(pareto.ppf(0.01, k2),pareto.ppf(0.99,k2),100)
 
 plt.figure()
-plt.hist(res32,align='mid',color='tan',edgecolor='moccasin',bins=100,density=True,stacked=True)
+plt.hist(res32,align='mid',color='tan',edgecolor='moccasin',bins=20,density=True,stacked=True)
 xmin, xmax = plt.xlim()
 ymin, ymax = plt.ylim()
 plt.plot(x2-1, pareto.pdf(x2, k2),'g-', lw=2,alpha=0.6)
+plt.ylim(ymin,ymax)
 plt.title("Pareto Distributed Histogram (k=2.5)")
 plt.xlabel("Classes")
-plt.ylabel("Frequency")
+plt.ylabel("Density")
 plt.show
 print('----Pareto with K = 2.5----')
 print('The theoretical mean is: {0}'.format(mean2))
@@ -181,13 +183,14 @@ anavar3 = np.var(res33)
 x3 = np.linspace(pareto.ppf(0.01, k3),pareto.ppf(0.99,k3),100)
 
 plt.figure()
-plt.hist(res33,align='mid',color='tan',edgecolor='moccasin',bins=100,density=True,stacked=True)
+plt.hist(res33,align='mid',color='tan',edgecolor='moccasin',bins=20,density=True,stacked=True)
 xmin, xmax = plt.xlim()
 ymin, ymax = plt.ylim()
 plt.plot(x3-1, pareto.pdf(x3, k3),'g-', lw=2,alpha=0.6)
+plt.ylim(ymin,ymax)
 plt.title("Pareto Distributed Histogram (k=3)")
 plt.xlabel("Classes")
-plt.ylabel("Frequency")
+plt.ylabel("Density")
 plt.show
 print('----Pareto with K = 3----')
 print('The theoretical mean is: {0}'.format(mean3))
@@ -205,13 +208,14 @@ anavar4 = np.var(res34)
 x4 = np.linspace(pareto.ppf(0.01, k4),pareto.ppf(0.99,k4),100)
 
 plt.figure()
-plt.hist(res34,align='mid',color='tan',edgecolor='moccasin',bins=100,density=True,stacked=True)
+plt.hist(res34,align='mid',color='tan',edgecolor='moccasin',bins=20,density=True,stacked=True)
 xmin, xmax = plt.xlim()
 ymin, ymax = plt.ylim()
 plt.plot(x4-1, pareto.pdf(x4, k4),'g-', lw=2,alpha=0.6)
+plt.ylim(ymin,ymax)
 plt.title("Pareto Distributed Histogram (k=4)")
 plt.xlabel("Classes")
-plt.ylabel("Frequency")
+plt.ylabel("Density")
 plt.show
 print('----Pareto with K = 4----')
 print('The theoretical mean is: {0}'.format(mean4))
--- Verkefni/verkefni5.py
+++ Verkefni/verkefni5.py
@@ -7,7 +7,7 @@ Created on Thu Jun  7 12:17:15 2018
 """
 
 import numpy as np
-import scipy.stats as st
+from scipy import stats
 
 #%%
 #• Estimate the integral exdx (from 0 to 1) by simulation (the crude Monte
@@ -46,7 +46,7 @@ theo_a = 0.0039
 #v = np.cov(np.exp(U))
 
 #Confidence interval
-z_star = st.t.ppf(0.975,n-1)
+z_star = stats.t.ppf(0.975,n-1)
 upper_a = Y_bar_a + z_star*(np.std(Y)/np.sqrt(n))
 lower_a = Y_bar_a - z_star*(np.std(Y)/np.sqrt(n))
 
@@ -73,7 +73,7 @@ Var_c = np.sum(Z**2)/n - (np.sum(Z)/n)**2
 theo_c = 0.0039
 
 #Confidence interval
-z_star = st.t.ppf(0.975,n-1)
+z_star = stats.t.ppf(0.975,n-1)
 upper_c = Z_bar + z_star*(np.std(Z)/np.sqrt(n))
 lower_c = Z_bar - z_star*(np.std(Z)/np.sqrt(n))
 
@@ -100,7 +100,7 @@ W_bar = np.sum(W)/n
 Var_s = np.sum(W**2)/n - (np.sum(W)/n)**2
 
 #Confidence interval
-z_star = st.t.ppf(0.975,n-1)
+z_star = stats.t.ppf(0.975,n-1)
 upper_w = W_bar + z_star*(np.std(W)/np.sqrt(n))
 lower_w = W_bar - z_star*(np.std(W)/np.sqrt(n))
 
@@ -115,3 +115,71 @@ print("The lower confidence interval with 95% certainty is: {}".format(lower_w))
 #%%
 #• Use control variates to reduce the variance of the estimator in
 #exercise 4 (Poisson arrivals).
+def queue_simulation(n_su, mst, mtbc, n_customers, arrival_dist, service_time_dist):
+    blocked_customer_count = 0
+    time = list(np.cumsum(arrival_dist))
+    service_unit_times = [0 for _ in range(n_su)]
+
+    for i in range(n_customers):
+        min_service_unit_time = min(service_unit_times)
+        if time[i] > min_service_unit_time:
+            service_unit_time = service_time_dist[i] + time[i]
+            service_unit_times[service_unit_times.index(min_service_unit_time)] = service_unit_time
+        else:
+            blocked_customer_count = blocked_customer_count + 1
+
+    return blocked_customer_count
+
+def event_simulation(n_su, mst, mtbc, n_sims, n_customers, arr_type, serv_type): 
+    results = []
+    arrival_dist_types = {
+                #interarrival intervals are exponentionally distributed when arrival process is a Poisson process
+                "poisson": lambda: stats.expon.rvs(size=n_customers, scale=mtbc),
+                #p_1 = 0.8, lambda_1 = 0.8333, p_2 = 0.2, lambda_2 = 5.0
+                "hyper_exp": lambda: [stats.expon.rvs(scale=1/0.8333) if u < 0.8 else stats.expon.rvs(scale=1/5) for u in list(np.random.random_sample(10000))]
+            }
+    service_time_dist_types = {
+                "exp": lambda: stats.expon.rvs(size=n_customers, scale=mst),
+                "constant": lambda: [mst for _ in range(n_customers)],
+                #k=1.05 or k=2.05 will make interesting choices
+                "pareto": lambda: [((mst*(2.05-1))/2.05) / (np.random.uniform(0,1)**(1/2.05)) for _ in range(n_customers)]
+            }
+    
+    for i in range(n_sims):
+        arrival_dist = arrival_dist_types[arr_type]()
+        service_time_dist = service_time_dist_types[serv_type]()
+        blocked_customer_count = queue_simulation(n_su, mst, mtbc, n_customers, arrival_dist, service_time_dist)
+        results.append(blocked_customer_count / n_customers)
+        
+    blocked_customers_fraction = sum(results) / n_sims
+    confidence_intervals = calculate_confidence_intervals(st.mean(results), st.stdev(results), n_sims)
+    
+    print("Simulation with the arrival process as {} and the service time distribution as {}".format(arr_type, serv_type))
+    print("-------------------------------------------------------------------------")
+    print("Percentage of blocked customers: {} %".format(blocked_customers_fraction * 100))
+    print("Confidence interval, lower limit: {} %".format(confidence_intervals[0] * 100))
+    print("Confidence interval, upper limit: {} %".format(confidence_intervals[1] * 100))
+    print("-------------------------------------------------------------------------")
+    print("\n")
+
+def event_simulation_poisson_arrival(n_su, mst, mtbc, n_sims, n_customers):    
+    event_simulation(n_su, mst, mtbc, n_sims, n_customers, "poisson", "exp")
+    
+def main():
+    #service units = 10, 
+    #mean service time = 8 time units, 
+    #mean timebetween customers = 1 time unit
+    #10 x 10.000 customers.
+    n_service_units = 10
+    mean_service_time = 8
+    mean_time_between_customers = 1
+    n_simulations = 10
+    n_customers = 10000
+    
+    #Simulation for when the arrival process is modelled as a Poisson process
+    event_simulation_poisson_arrival(n_service_units, mean_service_time, mean_time_between_customers, n_simulations, n_customers)
+    
+        #Exact solution
+    print("Exact solution, percentage of blocked customers: {} %".format(erlang_B_formula(n_service_units, mean_time_between_customers, mean_service_time)* 100))
+if __name__ == "__main__":
+    main()
